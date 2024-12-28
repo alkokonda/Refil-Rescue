8b5db293e1b930fafbc26c53f4cb09a9710c54e0
Anirudh <anirudh.why@gmail.com>
2024-12-28 18:23:12 +0530

    deployment-fix

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -11,7 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.json());
+
 app.use('/api/users', userRoutes);
+
+app.get((req, res, next) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
